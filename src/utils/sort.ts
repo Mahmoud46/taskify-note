6fbd092723c38847b465c8f6978fdfaf728956e2
mahmoudzakaria12 <mahmoudzsali247@gmail.com>
2025-06-28 13:36:04 +0300

    Update sorting priority favorite>type>status>time

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -16,21 +16,25 @@ const parseDateTime = (dateStr: string, timeStr: string): number => {
 
 const assignPriority = (item: INote): number => {
 	const statusPriority: Record<TTaskStatus, number> = {
-		upcoming: 0,
-		ongoing: 1,
+		ongoing: 0,
+		upcoming: 1,
 		completed: 2,
 		cancelled: 3,
 	};
 
 	const typeScore = item.type === "task" ? 0 : 1; // task before note
 	const favoriteScore = item.favorite ? 0 : 1; // favorite before non-favorite
-	const statusScore = item.status ? statusPriority[item.status] : -1; // status priority
+	const statusScore = item.status
+		? statusPriority[item.status as TTaskStatus]
+		: 4; // status priority
 	const timeScore =
-		item.date && item.time ? parseDateTime(item.date, item.time) : -1; // earlier time first
+		item.date && item.time
+			? parseDateTime(item.date, item.time)
+			: Number.MAX_SAFE_INTEGER; // earlier time first
 
 	// Compose into a weighted priority score
 	return (
-		typeScore * 1e13 + favoriteScore * 1e12 + statusScore * 1e10 + timeScore
+		favoriteScore * 1e13 + typeScore * 1e12 + statusScore * 1e10 + timeScore
 	);
 };
 
